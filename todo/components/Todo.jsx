d91d24e708d77f84452ac76cda0dd5afb3e4a1e5
ambuj vashistha <ambujva123@gmail.com>
2025-08-23 21:00:01 +0530

    added profile pic

--- todo/components/Todo.jsx
+++ todo/components/Todo.jsx
@@ -1,5 +1,5 @@
-import { react } from "react";
-import { View, Text, TextInput } from "react-native";
+import { react, useState } from "react";
+import { View, Text, TextInput, SafeAreaView, Image } from "react-native";
 
 const styles = {
   input: {
@@ -8,10 +8,26 @@ const styles = {
     borderRadius: 20,
     width: 250,
     height: 40,
+    margin: 5,
     padding: 30,
+    backgroundColor: "#F2F0EF",
+  },
+  profilePic: {
+    height: 55,
+    width: 55,
+    borderRadius: 100,
+    margin: 10,
   },
 };
 
 export default function Todo() {
-  return <TextInput style={styles.input} />;
+  const [profilePic, setproilePic] = useState(
+    "https://upload.wikimedia.org/wikipedia/en/2/21/Web_of_Spider-Man_Vol_1_129-1.png"
+  );
+  return (
+    <SafeAreaView style={{ flexDirection: "row" }}>
+      <Image style={styles.profilePic} source={{ uri: profilePic }} />
+      <TextInput style={styles.input}> </TextInput>
+    </SafeAreaView>
+  );
 }
